1ec234c33b2b7f2e01a01f22efc2389240904196
ShellTux <115948079+ShellTux@users.noreply.github.com>
2025-04-12 22:08:24 +0100

    Reset error count between rows. herpderp

--- src/args.py
+++ src/args.py
@@ -1,9 +1,10 @@
+from enum import StrEnum
 import argparse
-from enum import StrEnum, auto
+import gymnasium as gym
 
 parser = argparse.ArgumentParser(description='Lunar Lander')
 parser.add_argument(
-    '--render',
+    '--render-mode',
     choices=['none', 'human'],
     default='human',
     help="Set the render option to 'none' or 'human'."
@@ -33,15 +34,13 @@ parser.add_argument(
     help="Set the wind power."
 )
 parser.add_argument(
-    '--wind',
-    type=bool,
-    default=False,
+    '--enable-wind',
+    action='store_true',
     help="Enable the wind."
 )
 parser.add_argument(
     '--evolve',
-    type=bool,
-    default=True,
+    action='store_true',
     help="Enable Evolution."
 )
 parser.add_argument(
@@ -68,19 +67,20 @@ class RenderMode(StrEnum):
     RenderHuman = 'human'
 
 class Args:
-    render: RenderMode | None
+    render_mode: RenderMode | None
     episodes: int
     gravity: float
     turbulence_power: float
     wind_power: float
-    wind: bool
+    enable_wind: bool
     evolve: bool
     generations: int
+    epochs: int
     crossover_probability: float
     mutation_probability: float
 
     def __init__(self, args: argparse.Namespace) -> None:
-        assert args.render in RenderMode
+        assert args.render_mode in RenderMode
         assert type(args.episodes) == int
         assert type(args.gravity) == float
         assert type(args.turbulence_power) == float
@@ -92,15 +92,34 @@ class Args:
         assert type(args.mutation_probability) == float
         assert 0 <= args.mutation_probability <= 1
 
-        self.render = args.render if args.render != 'none' else None
+        self.render_mode = args.render_mode if args.render_mode != RenderMode.RenderNone else None
         self.episodes = args.episodes
         self.gravity = args.gravity
         self.turbulence_power = args.turbulence_power
         self.wind_power = args.wind_power
-        self.wind = args.wind
+        self.enable_wind = args.enable_wind
         self.evolve = args.evolve
-        self.generations = args.generations
+        self.epochs = self.generations = args.generations
         self.crossover_probability = args.crossover_probability
         self.mutation_probability = args.mutation_probability
+
+    def make_env(
+        self,
+        *,
+        render_mode: RenderMode | None = None,
+        gravity: float | None = None,
+        enable_wind: bool | None = None,
+        wind_power: float | None = None,
+        turbulence_power: float | None = None,
+    ) -> gym.Env:
+        return gym.make(
+            'LunarLander-v3',
+            render_mode=render_mode or self.render_mode,
+            continuous=True,
+            gravity=gravity or self.gravity,
+            enable_wind=enable_wind or self.enable_wind,
+            wind_power=wind_power or self.wind_power,
+            turbulence_power=turbulence_power or self.turbulence_power,
+        )
 
 args = Args(parser.parse_args())
